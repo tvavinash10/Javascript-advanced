3104a617ac3f2c99efb0addad55fddfb2c2cf55f
Avinash.T.V <tvavinash10@gmail.com>
2023-01-09 12:57:53 +0530

    Closure

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 //scope
 //Nested function scope
-let a =10
-function outer(){
-    let b =20
-    function inner(){
-        let c =30
-        console.log(a,b,c)
-    }
-    inner()
+// let a =10
+// function outer(){
+//     let b =20
+//     function inner(){
+//         let c =30
+//         console.log(a,b,c)
+//     }
+//     inner()
+// }
+// outer()
+//closure
+function outer() {
+  let counter = 0;
+  function inner() {
+    counter++;
+    console.log(counter);
+  }
+  return inner
 }
-outer()
+const fn = outer();
+fn()
+fn()
