9d35f342dd31b2d96c909bad51ce2e2eae7aa97a
Avinash.T.V <tvavinash10@gmail.com>
2023-01-10 15:35:15 +0530

    currying function

--- main.js
+++ main.js
@@ -11,14 +11,36 @@
 // }
 // outer()
 //closure
-function outer() {
-  let counter = 0;
-  function inner() {
-    counter++;
-    console.log(counter);
-  }
-  return inner
+// function outer() {
+//   let counter = 0;
+//   function inner() {
+//     counter++;
+//     console.log(counter);
+//   }
+//   return inner
+// }
+// const fn = outer();
+// fn()
+// fn()
+// function currying
+function sum(a, b, c) {
+  return a + b + c;
 }
-const fn = outer();
-fn()
-fn()
+// console.log(sum(2, 3, 5));
+
+function curry(fn){
+    return function(a){
+        return function(b){
+            return function(c){
+                return fn(a, b, c)
+            }
+        }
+    }
+}
+const curriedSum = curry(sum)
+console.log(curriedSum(2)(3)(5))
+
+const add2 = curriedSum(2)
+const add3 = add2(3)
+const add5 = add3(5)
+console.log(add5)
