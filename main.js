f2d43a5736586f8322005486eaaae65f67a7c0f9
Avinash.T.V <tvavinash10@gmail.com>
2023-01-11 21:29:12 +0530

    this keyword

--- main.js
+++ main.js
@@ -23,24 +23,53 @@
 // fn()
 // fn()
 // function currying
-function sum(a, b, c) {
-  return a + b + c;
-}
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
 // console.log(sum(2, 3, 5));
 
-function curry(fn){
-    return function(a){
-        return function(b){
-            return function(c){
-                return fn(a, b, c)
-            }
-        }
-    }
-}
-const curriedSum = curry(sum)
-console.log(curriedSum(2)(3)(5))
+// function curry(fn){
+//     return function(a){
+//         return function(b){
+//             return function(c){
+//                 return fn(a, b, c)
+//             }
+//         }
+//     }
+// }
+// const curriedSum = curry(sum)
+// console.log(curriedSum(2)(3)(5))
 
-const add2 = curriedSum(2)
-const add3 = add2(3)
-const add5 = add3(5)
-console.log(add5)
+// const add2 = curriedSum(2)
+// const add3 = add2(3)
+// const add5 = add3(5)
+// console.log(add5)
+// this
+// function sayMyName(name){
+//     console.log(`My name is ${name}`)
+// }
+// sayMyName('Walter White')
+// sayMyName('Heisenberg')
+// Implicit binding
+// Explicit binding
+// New binding
+// Default binding
+// const person = {
+// name:'Vishwas',
+// sayMyName: function(name){
+//         console.log(`My name is ${this.name}`)
+//     },
+// }
+// person.sayMyName()
+// const name = 'super man'
+globalThis.name = 'superman'
+function sayMyName(){
+        console.log(`My name is ${this.name}`)
+    }
+// 
+function person(name){
+    // this is {}
+    this.name = name
+}
+// const p1 = new person('avinash')
+// console.log(p1.name)
